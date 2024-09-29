4f965c4bd4f343f204fae5a209500a964c284582
so1ar <so1ar114514@gmail.com>
2024-09-29 13:02:01 +0800

    Correct Smoothfox

---
+++ Smoothfox.js
@@ -0,0 +1,11 @@
+// credit: https://github.com/black7375/Firefox-UI-Fix
+// only sharpen scrolling
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("mousewheel.min_line_scroll_amount", 10); // 10-40; adjust this number to your liking; default=5
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
+user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
+user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
+// Firefox Nightly only:
+// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
+user_pref("general.smoothScroll.msdPhysics.enabled", false); // [FF122+ Nightly]
--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ if ! [[ -d "$./custom-overrides" ]]; then
     mkdir custom-overrides
 fi
 
-# Download Peskyfox, Smoothfox and Fastfox
+# Download Peskyfox and Fastfox
 wget https://raw.githubusercontent.com/yokoffing/Betterfox/refs/heads/main/Peskyfox.js -O custom-overrides/Peskyfox.js
-wget https://raw.githubusercontent.com/yokoffing/Betterfox/refs/heads/main/Smoothfox.js -O custom-overrides/Smoothfox.js
 wget https://raw.githubusercontent.com/yokoffing/Betterfox/refs/heads/main/Fastfox.js -O custom-overrides/Fastfox.js
 
 # Download Arkenfox updater
@@ -17,6 +16,7 @@ wget https://gist.githubusercontent.com/SpacingBat3/d9b56e0ee88f7a7201e6a45f4858
 
 # Link user-overrides.js
 ln -sf $(pwd)/user-overrides.js $(pwd)/custom-overrides/user-overrides.js
+ln -sf $(pwd)/Smoothfox.js $(pwd)/custom-overrides/Smoothfox.js
 
 # Merge overrides
 bash updater.sh -s -o custom-overrides
